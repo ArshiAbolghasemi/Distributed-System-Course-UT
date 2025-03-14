67c07ba754446e6398e1366b371c4b1431a51dc6
ArshiAbolghasemi <arshiabolghasemi@gmail.com>
2025-03-14 16:30:01 +0330

    style: fix gofmt

--- kvmsg/util/slice.go
+++ kvmsg/util/slice.go
@@ -6,21 +6,20 @@ import (
 )
 
 func FindIndex(s [][]byte, target []byte) int {
-    for i, val := range s {
-        if bytes.Equal(val, target) {
-            return i
-        }
-    }
+	for i, val := range s {
+		if bytes.Equal(val, target) {
+			return i
+		}
+	}
 
-    return -1
+	return -1
 }
 
 func ReplaceElement[T any](s []T, i int, v T) []T {
-    if i < 0 || i >= len(s) {
+	if i < 0 || i >= len(s) {
 		return s
 	}
 
 	s = slices.Delete(s, i, i+1)
 	return append(s[:i], append([]T{v}, s[i:]...)...)
 }
-
