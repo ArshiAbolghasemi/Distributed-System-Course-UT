82373eca791f9aa52c3c288f098f765df7041ff1
ArshiAbolghasemi <arshiabolghasemi@gmail.com>
2025-03-14 16:29:41 +0330

    style: gofmt

--- kvmsg/internal/kvstore/kvstore_impl.go
+++ kvmsg/internal/kvstore/kvstore_impl.go
@@ -5,41 +5,40 @@ import (
 )
 
 type impl struct {
-    storage map[string][]([]byte)
+	storage map[string][]([]byte)
 }
 
 func New() KVStore {
-    return &impl{
-        storage: make(map[string][]([]byte)),
-    }
+	return &impl{
+		storage: make(map[string][]([]byte)),
+	}
 }
 
 func (im *impl) Put(key string, value []byte) {
-    im.storage[key] = append(im.storage[key], value)
+	im.storage[key] = append(im.storage[key], value)
 }
 
 func (im *impl) Get(key string) []([]byte) {
-    valueList := im.storage[key]
-    return valueList
+	valueList := im.storage[key]
+	return valueList
 }
 
 func (im *impl) Delete(key string) {
-    delete(im.storage, key)
+	delete(im.storage, key)
 }
 
 func (im *impl) Update(key string, oldVal []byte, newVal []byte) {
-    valueList, exist := im.storage[key]
-    if !exist {
-        im.Put(key, newVal)
-        return
-    }
+	valueList, exist := im.storage[key]
+	if !exist {
+		im.Put(key, newVal)
+		return
+	}
 
-    markedIndex := util.FindIndex(valueList, oldVal)
-    if markedIndex != -1 {
-        im.Put(key, newVal)
-        return
-    }
+	markedIndex := util.FindIndex(valueList, oldVal)
+	if markedIndex != -1 {
+		im.Put(key, newVal)
+		return
+	}
 
-    im.storage[key] = util.ReplaceElement(valueList, markedIndex, newVal)
+	im.storage[key] = util.ReplaceElement(valueList, markedIndex, newVal)
 }
-
