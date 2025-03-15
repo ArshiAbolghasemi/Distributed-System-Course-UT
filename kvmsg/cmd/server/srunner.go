3892032f2df8a951b9a32d9af86842b58bc4aac9
ArshiAbolghasemi <arshiabolghasemi@gmail.com>
2025-03-15 18:12:37 +0330

    fix: exist in case error

--- kvmsg/cmd/server/srunner.go
+++ kvmsg/cmd/server/srunner.go
@@ -14,7 +14,8 @@ import (
 func main() {
 	portStr, err := config.GetServerPort()
 	if err != nil {
-		fmt.Printf("Failed to load port config: %v", err)
+		fmt.Printf("Failed to load port config: %v\n", err)
+        os.Exit(1)
 	}
 
 	port, err := strconv.Atoi(portStr)
