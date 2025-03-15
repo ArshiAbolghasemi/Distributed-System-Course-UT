ba72a8318663ee173d66b0930107a9d312b20484
ArshiAbolghasemi <arshiabolghasemi@gmail.com>
2025-03-15 17:18:22 +0330

    feat: adding getter methods for configs

--- kvmsg/config/config.go
+++ kvmsg/config/config.go
@@ -6,6 +6,8 @@ import (
 	"github.com/spf13/viper"
 )
 
+const config_file_path = "./config.yml"
+
 type Config struct {
 	Server struct {
 		Host     string `mapstructure:"host"`
@@ -34,6 +36,38 @@ func LoadConfig(filename string) (Config, error) {
 	return c, nil
 }
 
-func (c Config) GetServerAddress() string {
-	return fmt.Sprintf("%s:%s", c.Server.Host, c.Server.Port)
+func GetServerAddress() (string, error) {
+    c, err := LoadConfig(config_file_path)
+    if err != nil {
+        return "", err
+    }
+
+	return fmt.Sprintf("%s:%s", c.Server.Host, c.Server.Port), nil
+}
+
+func GetServerPort() (string, error) {
+    c, err := LoadConfig(config_file_path)
+    if err != nil {
+        return "", err
+    }
+
+    return c.Server.Port, nil
+}
+
+func GetServerProtocol() (string, error) {
+    c, err := LoadConfig(config_file_path)
+    if err != nil {
+        return "", nil
+    }
+
+    return c.Server.Protocol, nil
+}
+
+func GetClientChanBufLimit() (int, error) {
+    c, err := LoadConfig(config_file_path)
+    if err != nil {
+        return -1, nil
+    }
+
+    return c.Client.ChannelBufferLimit, nil
 }
